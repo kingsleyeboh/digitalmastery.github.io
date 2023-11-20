a4fe83458422e07f57ba24056a82eadbdf371add
Kinguzo <101510592+kingsleyeboh@users.noreply.github.com>
2023-11-20 15:50:56 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -22,22 +22,22 @@ function joinClass() {
 
 
 
-function startTimer() {
-  var countDownDate = new Date("Nov 20, 2023 00:00:00").getTime(); // Replace with your desired date
+// function startTimer() {
+//   var countDownDate = new Date("Nov 20, 2023 00:00:00").getTime(); // Replace with your desired date
 
-  var x = setInterval(function () {
-      var now = new Date().getTime();
-      var distance = countDownDate - now;
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//   var x = setInterval(function () {
+//       var now = new Date().getTime();
+//       var distance = countDownDate - now;
+//       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      document.getElementById("timer").innerHTML = days + "day(s) " + hours + "hrs " + minutes + "min " + seconds + "secs ";
+//       document.getElementById("timer").innerHTML = days + "day(s) " + hours + "hrs " + minutes + "min " + seconds + "secs ";
 
-      if (distance < 0) {
-          clearInterval(x);
-          document.getElementById("timer").innerHTML = "EXPIRED";
-      }
-  }, 1000);
-}
+//       if (distance < 0) {
+//           clearInterval(x);
+//           document.getElementById("timer").innerHTML = "EXPIRED";
+//       }
+//   }, 1000);
+// }
